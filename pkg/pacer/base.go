136160d1e1e9d1e6db1798351a4d7f7704365e5b
cnderrauber <zengjie9004@gmail.com>
2023-11-28 11:03:42 +0800

    Check packetTime

--- pkg/pacer/base.go
+++ pkg/pacer/base.go
@@ -82,21 +82,25 @@ func (b *Base) writeRTPHeaderExtensions(p *Packet) (time.Time, error) {
 		p.Header.SetExtension(ext.ID, ext.Payload)
 	}
 
-	sendingAt := b.packetTime.Get()
-	if p.AbsSendTimeExtID != 0 {
-		sendTime := rtp.NewAbsSendTimeExtension(sendingAt)
-		b, err := sendTime.Marshal()
-		if err != nil {
-			return time.Time{}, err
+	if b.packetTime != nil {
+		sendingAt := b.packetTime.Get()
+		if p.AbsSendTimeExtID != 0 {
+			sendTime := rtp.NewAbsSendTimeExtension(sendingAt)
+			b, err := sendTime.Marshal()
+			if err != nil {
+				return time.Time{}, err
+			}
+
+			err = p.Header.SetExtension(p.AbsSendTimeExtID, b)
+			if err != nil {
+				return time.Time{}, err
+			}
 		}
 
-		err = p.Header.SetExtension(p.AbsSendTimeExtID, b)
-		if err != nil {
-			return time.Time{}, err
-		}
+		return sendingAt, nil
 	}
 
-	return sendingAt, nil
+	return time.Time{}, nil
 }
 
 // ------------------------------------------------
